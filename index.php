f8fb827c511683751c7d8c38cdb3cfec177ad85b
Elisha Ukpong <Ishukpong418@gmail.com>
2020-06-18 19:51:35 +0100

    new

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require 'partials/header.php';
             <div class="row">
             <div class="col-md-6 landing d-flex flex-column align-items-start justify-content-center">
                     <h1 class="head">Quickly Reward Interns <br> With Airtime</h1>
-                    <a class="btn btn-primary mt-5">Get Started</a>
+                    <a class="btn btn-primary mt-5" href="/send-airtime.php">Get Started</a>
                 </div>
                 <div class="col-md-6 d-flex justify-content-end">
                     <img src="./images/circles.png" alt="" class="img-fluid circle-img">
